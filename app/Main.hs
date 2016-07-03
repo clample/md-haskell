c42c374c7b7b69331094bd460f81d0400fb7bbf7
chrislample <clample@testobject.com>
2016-07-03 19:14:20 +0200

    Read and write from files

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,18 @@ module Main where
 
 import System.IO
 import System.Directory
+import System.Environment
 import MarkdownParser
 
 main :: IO ()
 main = do
-  handle <- openFile "src/sample-markdown.md" ReadMode
-  contents <- hGetContents handle
-  putStr "Good"
-  hClose handle
+  args <- getArgs
+  case args of
+    [] -> do putStrLn "Please specify an input and output path"
+    readPath:[] -> do putStrLn "Please specify an input and output path"
+    readPath:writePath:[] -> do readHandle <- openFile readPath ReadMode
+                                contents <- hGetContents readHandle
+                                writeHandle <- openFile (writePath) WriteMode
+                                hPutStr writeHandle (parseAndRenderHtml parseMarkdown contents)
+                                hClose readHandle
+                                hClose writeHandle
